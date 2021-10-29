d9986a12d2eae90f92c9065463001f23a8bab3e6
MuhammadZulfikarIkhsan8 <91503899+MuhammadZulfikarIkhsan8@users.noreply.github.com>
2021-10-29 19:57:07 +0700

    Update Main.java

--- 6706202049_Assesment1/Assesment1no1/src/com/zul/Main.java
+++ 6706202049_Assesment1/Assesment1no1/src/com/zul/Main.java
@@ -3,18 +3,19 @@ package com.zul;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // write your code here - Construktor di Main class
         // Asuransi dan Dropship
         new MarketPlace(1, "N0001", "JNE", 21000, "Dropship1", "Surabaya", "Bejo", "DayeuhKolot", true, true, 40000);
 
-        // Normal
+        // Asuransi
         new MarketPlace(2, "N0003", "J&T", 16000, "Eiger", "Bandung Kopo", "Tini", "DayeuhKolot", true, 40000);
 
-        // Asuransi
+        // Normal
         new MarketPlace(3, "N0004", "Pos Indonesia", 23000, "Eiger", "Bandung Kopo", "Bambang", "DayeuhKolot");
 
         // Dropship
         new MarketPlace(4, "N0002", "Si Cepat", 24000, "Dropship2", "Pontianak", "Joko", "DayeuhKolot", true);
+        // untuk menginisialisai Objek dengan keyword new
+        // Lalu membawa sebuah parameter
     }
-
 }
